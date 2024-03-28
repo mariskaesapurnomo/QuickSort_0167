d301ed509a327674a006d5772210c33394d9aaf9
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-28 10:46:07 +0700

    step 9 perbandingan jika i lebih kecil dari j

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -68,6 +68,11 @@ void q_short(int low, int high)
             j--; //step 8
             cmp_count++;
         }
+        cmp_count++;
+        if (i < j)  //step 9
+        {
+
+        }
     }
 }
 
