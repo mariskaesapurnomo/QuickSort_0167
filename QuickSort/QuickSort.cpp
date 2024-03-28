769755836d1d997226dd2125f4b68bccb5d6f8a5
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-28 10:25:04 +0700

    step 10  j sama dengan i

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -50,8 +50,13 @@ void q_short(int low, int high)
     }
 
     pivot = arr[low];   //step 2
-    i = low + 1;    //step 3
-    j = high;       //step 4
+    i = low + 1;       //step 3
+    j = high;         //step 4
+
+    while (i <= j)      //step 10
+    {
+
+    }
 }
 
 
