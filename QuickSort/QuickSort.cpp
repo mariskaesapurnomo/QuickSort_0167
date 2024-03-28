00584a0802f46f12250eab2e3436199317950ab0
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-28 10:39:07 +0700

    step 7 mencari sebuah element kurang dari sama dengan pivot

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -61,6 +61,12 @@ void q_short(int low, int high)
             i++; //step 6
             cmp_count++;
         }
+        cmp_count++;
+        //search for an element less than or equal to pivot
+        while ((arr[j] > pivot) && (j >= low))      //step 7
+        {
+
+        }
     }
 }
 
